ffba7563d99c5c77a694955c64b61a3f13fa6b70
Varu98 <vardaan@wisdomtreeventures.com>
2023-06-15 15:03:44 +0530

    add : Solve Question Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import * as React from "react";
 import { ChakraProvider, Text, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
+import SolveQuestion from "./components/SolveQuestion";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Text>Hello World</Text>
+    <SolveQuestion />
   </ChakraProvider>
 );
